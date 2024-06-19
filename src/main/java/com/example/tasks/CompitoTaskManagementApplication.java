14353a49d0d6a337470b203fc2aad3115cd1a294
vinay-parekh <bvp0802@gmail.com>
2024-06-19 11:07:19 +0530

    initial commit

---
+++ src/main/java/com/example/tasks/CompitoTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompitoTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompitoTaskManagementApplication.class, args);
+	}
+
+}
